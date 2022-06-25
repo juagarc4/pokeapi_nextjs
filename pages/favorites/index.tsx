6c56f4b1eae4f6d49931c2546a7f660ceb1c2f84
Raul Garcia <raulgarciacanet@gmail.com>
2022-06-25 15:47:54 +0200

    Finalizing favorites. Navigate to pokemon page from favorites. Symplifying Favorites page

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { Layout } from 'components/layouts'
 import { NoFavorites } from 'components/ui'
 import { localFavorites } from 'utils'
-import { Card, Grid } from '@nextui-org/react'
+import { FavoritePokemons } from 'components/pokemon'
 
 const FavoritesPage = () => {
   const [favoritePokemons, setFavoritePokemons] = useState<number[]>([])
@@ -13,23 +13,7 @@ const FavoritesPage = () => {
 
   return (
     <Layout title='Pokemon - Favoritos'>
-      {favoritePokemons.length === 0 ? (
-        <NoFavorites />
-      ) : (
-        <Grid.Container gap={2} direction='row' justify='flex-start'>
-          {favoritePokemons.map((id) => (
-            <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-              <Card hoverable clickable css={{ padding: 10 }}>
-                <Card.Image
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-                  width={'100%'}
-                  height={'140px'}
-                />
-              </Card>
-            </Grid>
-          ))}
-        </Grid.Container>
-      )}
+      {favoritePokemons.length === 0 ? <NoFavorites /> : <FavoritePokemons pokemons={favoritePokemons} />}
     </Layout>
   )
 }
