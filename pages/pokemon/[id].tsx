2baeb2e64b4c88123ca99241f03cf5dcc2736c7f
Raul Garcia <raulgarciacanet@gmail.com>
2022-06-27 16:23:44 +0200

    Add ISR

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -88,6 +88,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       pokemon: await getPokemonInfo(id),
     },
+    revalidate: 60,
   }
 }
 export default PokemonPage
